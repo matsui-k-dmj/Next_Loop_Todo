e36eba49aa625143177778f994e8e6c0b39717ee
matsui-k <osskil611@gmail.com>
2021-12-16 15:34:27 +0900

    init Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import { css } from "@emotion/react";
 const styles = {
   container: css`
     display: flex;
-    background-color: #6c9;
+    gap: 1rem;
   `,
 };
 
@@ -11,14 +11,13 @@ type NavbarProps = {
   selectedFeature?: "todo" | "routines";
 };
 
-export default function Navbar({ selectedFeature = undefined }: NavbarProps) {
+export default function Navbar({ selectedFeature }: NavbarProps) {
   selectedFeature;
   return (
     <>
       <div css={styles.container}>
         <div>Todo</div>
         <div>Routines</div>
-        <div>Log</div>
       </div>
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
-import { css } from "@emotion/react";
 import Navbar from "../components/Navbar";
-
-const anotherClass = css`
-  background-color: #c69;
-`;
 
 export default function Home() {
   return (
     <>
       <Navbar></Navbar>
-      <div css={anotherClass}>Zero runtime CSS in JS</div>
     </>
   );
 }
