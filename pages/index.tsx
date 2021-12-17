a530a368027591ef638cc24bfce517078e9e9fa6
matsui-k <osskil611@gmail.com>
2021-12-17 22:15:00 +0900

    Todo で日付の表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Navbar from "components/Navbar";
-
+import { format } from "date-fns";
+import ja from "date-fns/locale/ja";
 export default function Home() {
   return (
     <>
       <Navbar selectedFeature="todo"></Navbar>
+      <div>
+        <h2>{format(new Date(), "M/d E", { locale: ja })}</h2>
+      </div>
     </>
   );
 }
