84844c4ed3d4d162ee62a3aa2bc1496a2055f500
matsui-k <osskil611@gmail.com>
2021-12-22 02:25:34 +0900

    connect auth emulator

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, connectAuthEmulator } from "firebase/auth"
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_apiKey,
     authDomain: process.env.NEXT_PUBLIC_authDomain,
@@ -14,3 +11,5 @@ const firebaseConfig = {
 }
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+connectAuthEmulator(auth, "http://localhost:9099");
